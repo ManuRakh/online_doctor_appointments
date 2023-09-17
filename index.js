b737d27ad069d0ead981c3d5a2a16d5f3265eecb
Manuchekhr Rakhimov <30647642+ManuRakh@users.noreply.github.com>
2023-09-17 16:21:10 +0300

    Fixed bugs

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const token = '6608693910:AAHf1Wam86Zr4m2kqjIrkuVweB7PabnG8M0';
 const db = new Database('hospital.db');
 const bot = new TelegramBot(token, { polling: true });
 
+const steps = {
+    1: { name: 'Выбор специальности', action: selectSpecialty },
+    2: { name: 'Выбор поликлиники', action: selectHospital },
+    3: { name: 'Выбор доктора', action: selectDoctor },
+    4: { name: 'Выбор времени', action: selectAppointmentTime },
+    5: { name: 'Завершение', action: createAppointment },
+    0: { name: 'Начало', action: "start" },
+}
+
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
+    let step = 0;
     const firstName = msg.from.first_name;
     const specialties = await db.getDoctorSpecialties();
 
@@ -28,24 +38,30 @@ bot.onText(/\/start/, async (msg) => {
       
     await db.createUser(name, chatId)
     bot.sendMessage(chatId, `Привет, ${firstName}! Пожалуйста, выберите специальность врача из списка:`, options);
-    await selectSpecialty(chatId);
+    await selectSpecialty(chatId, step);
   });
     }
     
     if (user) {
   bot.sendMessage(chatId, `Привет, ${firstName}! Пожалуйста, выберите специальность врача из списка:`, options);
-  await selectSpecialty(chatId);
+  await selectSpecialty(chatId, step);
 }        
 });
 
-async function selectSpecialty(chatId) {
+async function selectSpecialty(chatId, step = 0) {
+    const newStep = 2;
     bot.once('message', (msg) => {
-        const specialty = msg.text;
-        selectHospital(chatId, specialty);
+        if (step < newStep) step = newStep;
+        else {
+            console.log("Not by the step, step selectSpeciality", { step, newStep });
+            return;
+        }        const specialty = msg.text;
+        selectHospital(chatId, specialty, step);
     });
 }
 
-async function selectHospital(chatId, specialty) {
+async function selectHospital(chatId, specialty, step = 0) {
+    const newStep = 3;
     const supportedHospitals = await db.getHospitalsBySpecialty(specialty);
     const keyboard = supportedHospitals.map((hospital) => [{ text: hospital?.name }]);
     const options = {
@@ -57,13 +73,19 @@ async function selectHospital(chatId, specialty) {
     bot.sendMessage(chatId, `Отлично, вы выбрали специальность ${specialty}. Вот список поликлинник, где у нас есть доктора по выбранной вами специальности!:`, options);
 
     bot.once('message', async (msg) => {
+        if (step < newStep) step = newStep;
+        else {
+            console.log("Not by the step, step selectHospital", { step, newStep });
+            return;
+        }
         const hospital = msg.text;
         let hospitalId = supportedHospitals.find((h) => h.name === hospital)?.id;
-        selectDoctor(chatId, hospitalId, specialty);
+        selectDoctor(chatId, hospitalId, specialty, step);
     });
 }
 
-async function selectDoctor(chatId, hospital, specialty) {
+async function selectDoctor(chatId, hospital, specialty, step = 0) {
+    const newStep = 4;
     const getDoctorsBySpecialityAndHospital = await db.getDoctorsBySpecialityAndHospital(specialty, hospital);
     const keyboard = getDoctorsBySpecialityAndHospital.map((doctor) => [{ text: doctor?.name }]);
     const options = {
@@ -74,13 +96,20 @@ async function selectDoctor(chatId, hospital, specialty) {
     };
     bot.sendMessage(chatId, `Отлично, теперь давайте выберем к какому доктору записаться на приём :`, options);
     bot.once('message', async (msg) => {
+        if (step < newStep) step = newStep;
+        else {
+            console.log("Not by the step, step selectDoctor", { step, newStep });
+            return;
+        }
+
         const selectedDoctor = msg.text;
         const foundDoctor = getDoctorsBySpecialityAndHospital.find((d) => d.name === selectedDoctor);
-        selectAppointmentTime(chatId, hospital, specialty, foundDoctor);
+        selectAppointmentTime(chatId, hospital, specialty, foundDoctor, step);
     });
 }
 
-function selectAppointmentTime(chatId, hospitalId, specialty, foundDoctor) {
+function selectAppointmentTime(chatId, hospitalId, specialty, foundDoctor, step = 0) {
+    const newStep = 5;
     const availableTimes = foundDoctor.working_times.length ? JSON.parse(foundDoctor.working_times) : [];
 
     const keyboard = {
@@ -96,6 +125,11 @@ function selectAppointmentTime(chatId, hospitalId, specialty, foundDoctor) {
 
     bot.sendMessage(chatId, 'Выберите время приема:', keyboard);
     bot.once('message', async (msg) => {
+        if (step < newStep) step = newStep;
+        else {
+            console.log("Not by the step, step selectAppointmentTime", { step, newStep });
+            return;
+        }
         const selectedTime = msg.text;
         createAppointment(chatId, selectedTime);
     });
